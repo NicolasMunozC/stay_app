fe2bc4bd286ac11b384cb2a172150942c372e26b
nicolas_munoz <nmunozc94@gmail.com>
2023-11-13 18:45:15 -0300

    feat: add next themes provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <NextThemesProvider attribute="class" defaultTheme="light">
+        <App />
+      </NextThemesProvider>
     </NextUIProvider>
   </React.StrictMode>,
 );
